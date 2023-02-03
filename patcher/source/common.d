62fac94b78b7ddbd492d9c9f7588311bfb994645
Sofia N. Lima <me@dzshn.xyz>
2023-02-03 04:26:03 -0300

    fix patcher for windows again

--- patcher/source/common.d
+++ patcher/source/common.d
@@ -9,23 +9,23 @@ import std.process : environment;
 string guessPath() {
     // TODO: find more of these
     version (Windows) {
-        const localAppData = environment.get("LOCALAPPDATA");
-        const paths = [
+        auto localAppData = environment.get("LOCALAPPDATA");
+        auto paths = [
             localAppData.buildPath("Programs", "tetrio-desktop")
         ];
     } else version (OSX) {
-        const paths = [
+        auto paths = [
             "~/Applications".expandTilde,
         ];
     } else version (Posix) {
-        const paths = [
+        auto paths = [
             "/opt/TETR.IO"
         ];
     } else {
-        const string[] paths = [];
+        auto string[] paths = [];
     }
 
-    static foreach (path; paths) {
+    foreach (path; paths) {
         if (path.exists) {
             writeln("Found path ", path);
             return path;
