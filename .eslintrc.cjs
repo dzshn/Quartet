f7e172aaaf2dfb184bc822faa50b69e836f1eff9
Sofia Lima <me@dzshn.xyz>
2023-02-11 01:46:46 -0300

    chore: license headers on all files (eslint-plugin-header)

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -11,12 +11,16 @@ module.exports = {
         "svelte3",
         "@typescript-eslint",
         "simple-import-sort",
+        "header",
     ],
     globals,
     overrides: [
         {
             files: ["*.svelte"],
             processor: "svelte3/svelte3",
+            rules: {
+                "header/header": "off",
+            }
         },
     ],
     settings: {
@@ -46,7 +50,31 @@ module.exports = {
         "dot-notation": "error",
         "eol-last": ["error", "always"],
         "eqeqeq": ["error", "always", { null: "ignore" }],
+        "header/header": [
+            "error",
+            "block", [
+                "",
+                " * Quartet, a client mod for TETR.IO",
+                { pattern: " \\* Copyright \\(c\\) \\d{4}", template: ` * Copyright (c) ${new Date().getFullYear()} Sofia Lima and contributors` },
+                " *",
+                " * This program is free software: you can redistribute it and/or modify",
+                " * it under the terms of the GNU General Public License as published by",
+                " * the Free Software Foundation, either version 3 of the License, or",
+                " * (at your option) any later version.",
+                " *",
+                " * This program is distributed in the hope that it will be useful,",
+                " * but WITHOUT ANY WARRANTY; without even the implied warranty of",
+                " * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the",
+                " * GNU General Public License for more details.",
+                " *",
+                " * You should have received a copy of the GNU General Public License",
+                " * along with this program.  If not, see <https://www.gnu.org/licenses/>.",
+                " "
+            ],
+            2
+        ],
         "indent": ["error", 4],
+        "linebreak-style": ["error", "unix"],
         "no-duplicate-imports": "error",
         "no-self-compare": "error",
         "no-trailing-spaces": "error",
--- scripts/build.ts
+++ scripts/build.ts
@@ -1,3 +1,21 @@
+/*
+ * Quartet, a client mod for TETR.IO
+ * Copyright (c) 2023 Sofia Lima and contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 import { execSync } from "node:child_process";
 import { readdir, readFile } from "node:fs/promises";
 import process from "node:process";
@@ -10,6 +28,26 @@ const watch = process.argv.includes("--watch");
 const web = process.argv.includes("--web");
 const gitHash = execSync("git rev-parse --short HEAD", { encoding: "utf-8" }).trim();
 const version = `${process.env.npm_package_version} (${gitHash})`;
+
+const license = dedent(`
+    /*
+     * Quartet, a client mod for TETR.IO
+     * Copyright (c) 2023 Sofia Lima and contributors
+     *
+     * This program is free software: you can redistribute it and/or modify
+     * it under the terms of the GNU General Public License as published by
+     * the Free Software Foundation, either version 3 of the License, or
+     * (at your option) any later version.
+     *
+     * This program is distributed in the hope that it will be useful,
+     * but WITHOUT ANY WARRANTY; without even the implied warranty of
+     * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+     * GNU General Public License for more details.
+     *
+     * You should have received a copy of the GNU General Public License
+     * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+     */
+`);
 
 const sveltePreprocessor = sveltePreprocess({
     async typescript({ content }) {
@@ -82,12 +120,10 @@ const esbuildOpts: esbuild.BuildOptions = {
              * Quartet, a cute and minimal client mod for TETR.IO
              *
              * @author dzshn (https://dzshn.xyz)
-             * @license GPL-3.0
+             * @license GPL-3.0-or-later
              * @version ${version}
-             *
-             * @see {@link https://github.com/dzshn/Quartet}
              */
-        `),
+        `) + license,
     },
 };
 
@@ -113,7 +149,7 @@ function simplifyInputs(obj: esbuild.Metafile | esbuild.Metafile["outputs"][stri
     ));
 }
 
-async function main () {
+async function main() {
     const contexts: esbuild.BuildContext[] = [];
 
     if (web) {
@@ -145,12 +181,12 @@ async function main () {
                     // @description  A cute and minimal TETR.IO client mod
                     // @version      ${version}
                     // @author       dzshn (https://dzshn.xyz)
-                    // @license      GPL-3.0
+                    // @license      GPL-3.0-or-later
                     // @donate       https://ko-fi.com/dzshn
                     // @match        *://tetr.io/*
                     // @run-at       document-start
                     // ==/UserScript==
-                `),
+                `) + license,
             },
             footer: {
                 js: "Object.defineProperty(unsafeWindow,'Quartet',{get:()=>Quartet})\n//# sourceURL=Quartet"
